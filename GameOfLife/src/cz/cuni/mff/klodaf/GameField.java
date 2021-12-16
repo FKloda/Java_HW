fff4765ef8260a78093cf5605ef06f5204dc15e8
fra.kloda@gmail.com <fra.kloda@gmail.com>
2021-12-16 01:03:19 +0100

    Trying to implement threading management.

--- GameOfLife/src/cz/cuni/mff/klodaf/GameField.java
+++ GameOfLife/src/cz/cuni/mff/klodaf/GameField.java
@@ -2,30 +2,44 @@ package cz.cuni.mff.klodaf;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.RecursiveAction;
 
-public class GameField {
-    Boolean[][] first, second;
-    int numOfSteps, tableSize;
-    BufferedReader reader;
-    public GameField(int numOfSteps, int tableSize, BufferedReader reader){
-        this.numOfSteps = numOfSteps;
-        first = new Boolean[tableSize][tableSize];
-        this.reader = reader;
+public class GameField implements Runnable {
+    boolean[][] inTable, outTable;
+    int tableSize, indexX, indexY;
+    public GameField( int indexX, int indexY, boolean[][] inTable, boolean[][]outTable){
+        this.inTable = inTable;
+        this.outTable = outTable;
         this.tableSize = tableSize;
+        this.indexX = indexX;
+        this.indexY = indexY;
     }
-    public void readTheInput() throws IOException {
-        String input;
-        for (int x = 0; x < tableSize; x++) {
-            input = reader.readLine();
-            for (int y = 0; y < tableSize; y++){
-                if (input.charAt(y) == 'X'){
-                    first[x][y] = true;
-                }
+    @Override
+    public void run() {
+        try {
+            int numOfAlive;
+            int[] directions = new int[]{-1,-1,-1,0,-1,1,0,-1,0,1,1,-1,1,0,1,1};
+            if (indexX == 0){
+                directions[0] = directions[2] = directions[4] = tableSize-1;
             }
-        }
-    }
-    public void RunTheGame() throws IOException{
-        readTheInput();
+            if (indexY == 0){
+                directions[1] = directions[7] = directions[11] = tableSize-1;
+            }
+            if (indexX == tableSize-1){
+                directions[0] = directions[2] = directions[4] = -tableSize-1;
+            }
+            if (indexY == tableSize-1){
+                directions[1] = directions[7] = directions[11] = -tableSize-1;
+            }
 
+            Main.barrier.await();
+        }
+        catch (InterruptedException e){
+
+        }
+        catch (BrokenBarrierException e){
+
+        }
     }
 }
--- GameOfLife/src/cz/cuni/mff/klodaf/Main.java
+++ GameOfLife/src/cz/cuni/mff/klodaf/Main.java
@@ -4,34 +4,62 @@ import java.io.BufferedReader;
 import java.io.Console;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
+    public static CyclicBarrier barrier;
     public static void main(String[] args){
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String param;
         String[] paramArray;
         int tableSize, numOfSteps;
-        GameField game;
+        boolean[][] first, second;
+        ExecutorService executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
         try{
             param=reader.readLine();
             paramArray = param.split(" ");
             tableSize = Integer.parseInt(paramArray[0]);
             numOfSteps = Integer.parseInt(paramArray[1]);
+            first = readTheInput(tableSize, reader);
+            second = first.clone();
         }
         catch (IOException ex){
+            System.out.println("Error in input reading");
             return;
         }
         catch (NumberFormatException ex){
             System.out.println("First row must contain two Integers");
             return;
         }
-        game = new GameField(numOfSteps, tableSize, reader);
-        try{
-            game.RunTheGame();
+        barrier = new CyclicBarrier(tableSize*tableSize);
+        for(int p = 0; p<numOfSteps; p++) {
+            barrier.reset();
+            for (int x = 0; x < tableSize; x++) {
+                for (int y = 0; y < tableSize; y++) {
+                    if (p % 2 == 0) {
+                        executor.execute(new GameField(x, y, first, second));
+                    }
+                    else{
+                        executor.execute(new GameField(x,y,second,first));
+                    }
+                }
+            }
         }
-        catch (IOException ex){
-            System.out.println("Error in input");
-            return;
+
+    }
+    static boolean[][] readTheInput( int tableSize, BufferedReader reader) throws IOException {
+        String input;
+        boolean[][] table = new boolean[tableSize][tableSize];
+        for (int x = 0; x < tableSize; x++) {
+            input = reader.readLine();
+            for (int y = 0; y < tableSize; y++){
+                if (input.charAt(y) == 'X'){
+                    table[x][y] = true;
+                }
+            }
         }
+        return table;
     }
 }
